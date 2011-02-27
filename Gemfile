bba50ecf1d3075d4151752f8b95364c59dea1235
Christoph König <christoph.koenig@sales-lentz.lu>
2011-02-27 15:45:48 +0100

    Rails 3 app erstellt

---
+++ Gemfile
@@ -0,0 +1,7 @@
+# A sample Gemfile
+source 'http://rubygems.org'
+
+gem 'jquery-rails'
+gem 'mysql2'
+gem 'rails'
+gem 'haml'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JqmApp::Application.config.secret_token = 'e69035e28d463776a7db3fbbd270598673fbdc8009baee3e52c6ca2125ddd06f8f57104e922b9cff3b3be9082296020b87ffcad6bce9a60de6099318e1dbba59'
